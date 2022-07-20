f3d8bb819ae0d9229396605680f8db7eb390681d
nayoung188 <naya1649@naver.com>
2022-07-20 12:12:26 +0900

    2022-07-20

---
+++ javastudy/01_VARIABLE/src/ex05_string/Ex01.java
@@ -0,0 +1,29 @@
+package ex05_string;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		// 문자열 연결 연산자
+		// 문자열이 포함된 + 연산은 연결
+		
+		String str1 = "구디" + "아카데미";
+		String str2 = 100 + "번지";
+		String str3 = 1 + 1 + "행사"; 
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		
+	
+		// + 연산자를 이용한 문자열 만들기
+		// 빈 문자열("") 을 + 해 줌
+		String str4 = 100 + "";  // String.valueof(100) 과 동일
+	
+		System.out.println(str4);
+		
+		
+
+	}
+
+}
