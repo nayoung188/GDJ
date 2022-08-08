1677d4ae3ef4c7bb73a37bd5d97eacd4069b5189
nayoung188 <naya1649@naver.com>
2022-08-08 17:50:09 +0900

    2022-08-08

--- javastudy/09_COLLECTION/src/ex04_hashmap/Main.java
+++ javastudy/09_COLLECTION/src/ex04_hashmap/Main.java
@@ -1,6 +1,8 @@
 package ex04_hashmap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -64,8 +66,71 @@ public class Main {
 		System.out.println(map2);
 		
 	}
+	
+	public static void m3() {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("title", "소나기");
+		map.put("author", "황순원");
+		map.put("price", 20000);
+		
+		// Entry 단위로 순회 (for)
+		for(Map.Entry<String, Object> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());			
+		}
+		
+		
+		// Key를 이용한 순회 (for)
+		for(String key : map.keySet()) {
+			System.out.println(key + " : " + map.get(key));
+		}
+		
+	}
+	
+	public static void m4() {
+		
+		// 연습
+		// title, author, price 정보를 가진 임의의 Map 3개를 만들고,
+		// 생성된 Map 3개를 ArrayList에 저장한 뒤
+		// ArrayList에 저장된 Map 3개를 for문으로 순회하시오.
+		
+		Map<String, Object> map1 = new HashMap<String, Object>();
+		
+		map1.put("title", "소나기");
+		map1.put("author", "황순원");
+		map1.put("price", 10000);
+		
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		
+		map2.put("title", "어린왕자");
+		map2.put("author", "생텍쥐베리");
+		map2.put("price", 10000);
+		
+		Map<String, Object> map3 = new HashMap<String, Object>();
+		
+		map3.put("title", "홍길동전");
+		map3.put("author", "허균");
+		map3.put("price", 30000);
+		
+		List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+		list.add(map1);
+		list.add(map2);
+		list.add(map3);
+		
+		for(Map<String, Object> map : list) {   // List의 for문
+			for(Map.Entry<String, Object> entry : map.entrySet()) {   // Map의 for문
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+	
 	public static void main(String[] args) {
-		m2();
+		m4();
 	}
 
 }
