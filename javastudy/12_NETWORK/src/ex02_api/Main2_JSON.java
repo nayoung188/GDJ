08b2733a8bde18eda51d8b492fb2efffbb346f62
nayoung188 <naya1649@naver.com>
2022-08-19 17:24:08 +0900

    2022-08-19

--- javastudy/12_NETWORK/src/ex02_api/Main2_JSON.java
+++ javastudy/12_NETWORK/src/ex02_api/Main2_JSON.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.XML;
 
 public class Main2_JSON {
 
@@ -311,8 +312,6 @@ public class Main2_JSON {
 		return response;
 		
 	}
-
-			
 	
 	public static void m6() {
 		System.out.println(m5());
@@ -339,8 +338,86 @@ public class Main2_JSON {
 		System.out.println(map);		
 	}
 	
+	public static String m7() {
+		// 기상청 RSS
+		
+		
+				// 제주특별자치도 서귀포시 중문동
+				String apiURL = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=5013061000";
+				
+				// 접속	
+				URL url = null;
+				HttpURLConnection con = null;
+				
+				try {
+					
+					url = new URL(apiURL);
+					con = (HttpURLConnection)url.openConnection();
+				
+				}catch (IOException e) {
+					System.out.println("접속 실패");
+				}
+				
+				
+				// 응답 스트림 생성 및 응답 데이터 받기
+				BufferedReader reader = null;
+				StringBuilder sb = new StringBuilder();
+				
+				try {
+					if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+						reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					} else {
+						reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+					}
+					
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+						sb.append(line);
+					}
+					reader.close();
+				} catch (IOException e) {
+					System.out.println("응답 실패");
+				}
+				
+				
+				// XML 파일 생성
+			
+				File file = new File ("C:\\storage", "api4.xml");
+				
+				try {
+					BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+					writer.write(sb.toString());
+					writer.close();
+							
+				}catch(IOException e) {
+					System.out.println("파일 생성 실패");
+				}		
+		return sb.toString();
+	}
+	A
+	public static void m8() {
+		
+		// XML 문서를 JSONObject로 변환
+		JSONObject obj = XML.toJSONObject(m7());		
+
+
+		JSONArray dataList = obj.getJSONObject("rss")
+				.getJSONObject("channel")
+				.getJSONObject("item")
+				.getJSONObject("description")
+				.getJSONObject("body")
+				.getJSONArray("data");
+		
+		for (int i = 0 ; i < dataList.length(); i++) {
+			JSONObject weather = dataList.getJSONObject(i);
+			System.out.println(weather.getInt("hour") + "시 : " + weather.getInt("temp") + "도 , " + weather.getString("wfKor"));
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		m6();
+		m8();
 
 	}
 
