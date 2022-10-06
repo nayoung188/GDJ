1b6c8abec74bb98a8245dfd550b2ad8503566427
nayoung188 <naya1649@naver.com>
2022-10-06 19:49:37 +0900

    2022-10-06

--- jspstudy/01_Servlet/src/main/java/ex03/LocationServlet.java
+++ jspstudy/01_Servlet/src/main/java/ex03/LocationServlet.java
@@ -9,46 +9,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/AnchorServlet")
-public class AnchorServlet extends HttpServlet {
+
+@WebServlet("/LocationServlet")
+public class LocationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    
-    public AnchorServlet() {
-        super();
-        
-    }
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		// 요청
 		request.setCharacterEncoding("UTF-8");
 		
-		// 요청 파라미터 a, b
-		int a = 0;
-		int b = 0;
-		String strA = request.getParameter("a");
-		if( strA != null) {
-			a = Integer.parseInt(strA);
-		}
-		String strB = request.getParameter("b");
-		if( strB != null) {
-			b = Integer.parseInt(strB);
-		}
+		String param = request.getParameter("param");
+		
 		// 응답
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<h1>Hello World</h1>");
-		out.println("<h1>" + a + "+" + b  + " = " + (a+b) + "</h1>");
-		
-		out.flush();
+		out.println("<h1>안녕하세요 " + param + "님</h1>");
 		out.close();
-		
-		
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
