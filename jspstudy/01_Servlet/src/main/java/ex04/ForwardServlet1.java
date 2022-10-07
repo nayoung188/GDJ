ac5bdbf78b4f654bb2b7b5c5ed855441f45715c8
nayoung188 <naya1649@naver.com>
2022-10-07 16:44:19 +0900

    2022-10-07

--- jspstudy/01_Servlet/src/main/java/ex04/ForwardServlet1.java
+++ jspstudy/01_Servlet/src/main/java/ex04/ForwardServlet1.java
@@ -1,29 +1,27 @@
 package ex04;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/RedirectServlet")
-public class RedirectServlet extends HttpServlet {
+@WebServlet("/ForwardServlet1")
+public class ForwardServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-
+       
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// redirect 하는 방법
-		response.sendRedirect("/01_Servlet/RedirectServlet2");
-		
+		// Forward
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/ForwardServlet2");
+		requestDispatcher.forward(request, response);
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
---
+++ jspstudy/01_Servlet/src/main/java/ex04/RedirectServlet1.java
@@ -0,0 +1,29 @@
+package ex04;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/RedirectServlet1")
+public class RedirectServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// redirect 하는 코드 (외우기)
+		response.sendRedirect("/01_Servlet/RedirectServlet2");
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
