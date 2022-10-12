2ab0ad8c7424205e6c2ef4acd7d7e16f6b5abaff
nayoung188 <naya1649@naver.com>
2022-10-12 17:44:22 +0900

    2022-10-12

---
+++ webstudy/UI/login1.js
@@ -0,0 +1,29 @@
+/*
+비밀번호 입력 없이 Sign In 버튼 누르면
+경고창 띄우기 (비밀번호를 입력하세요.)
+*/
+
+document.getElementById('btn_signin').onclick = function(event){
+    var pw = document.getElementById('pw');
+    if(pw.value == ''){
+        alert('비밀번호를 입력하세요.');
+        event.preventDefault();    // 이벤트의 서브밋을 막는다.
+        return;
+    }
+}
+
+document.getElementById('id').onkeyup = function(event){
+    var id = document.getElementById('id');
+    var id_msg = document.getElementById('id_msg');
+    if(id.value.length == 0) {
+        id_msg.textContent = '';
+    } else if (id.value.length < 4 ) {
+        id_msg.textContent = '아이디는 4자 이상입니다.';
+        id_msg.style.color = '#FF0000';
+        id_msg.style.fontSize = '14px';
+    } else if ( id.value.length >= 4) {
+        id_msg.textContent = '정상적인 아이디입니다.';
+        id_msg.style.color = '#0000FF';
+        id_msg.style.fontSize = '14px';
+    }
+}
