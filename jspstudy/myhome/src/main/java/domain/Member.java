4e935bbb9d9eeae5e42d265a6d0d14e7afdef5cd
nayoung188 <naya1649@naver.com>
2022-10-26 19:38:14 +0900

    2022-10-26

---
+++ jspstudy/myhome/src/main/java/domain/Member.java
@@ -0,0 +1,15 @@
+package domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Member {
+	
+	
+	private int memberNo;
+	private String id, pw, name, email, registedDate;
+	
+
+}
