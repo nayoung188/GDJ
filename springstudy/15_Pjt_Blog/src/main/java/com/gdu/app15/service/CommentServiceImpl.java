276110ef8947cdff27395f69aa3037891b7465fb
nayoung188 <naya1649@naver.com>
2022-11-28 14:49:45 +0900

    2022-11-28

--- springstudy/15_Pjt_Blog/src/main/java/com/gdu/app15/service/CommentServiceImpl.java
+++ springstudy/15_Pjt_Blog/src/main/java/com/gdu/app15/service/CommentServiceImpl.java
@@ -1,15 +1,58 @@
 package com.gdu.app15.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gdu.app15.domain.CommentDTO;
 import com.gdu.app15.mapper.CommentMapper;
+import com.gdu.app15.util.PageUtil;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 
 	@Autowired
 	private CommentMapper commentMapper;
+	@Autowired
+	private PageUtil pageUtil;
 	
+	@Override
+	public Map<String, Object> getCommentCount(int blogNo) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("commentCount", commentMapper.selectCommentCount(blogNo));
+		return result;
+	}
+	
+	@Override
+	public Map<String, Object> addComment(CommentDTO comment) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("isAdd", commentMapper.insertComment(comment) == 1);		// 삽입의 결과가 1이면 true
+		return result;
+	}
+	
+	@Override
+	public Map<String, Object> getCommentList(HttpServletRequest request) {
+		int blogNo = Integer.parseInt(request.getParameter("blogNo"));
+		int page = Integer.parseInt(request.getParameter("page"));
+		
+		// 전체 comment 갯수
+		int commentCount = commentMapper.selectCommentCount(blogNo);
+		pageUtil.setPageUtil(page, commentCount);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("blogNo", blogNo);
+		map.put("begin", pageUtil.getBegin());
+		map.put("end", pageUtil.getEnd());
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("commentList", commentMapper.selectCommentList(map));
+		result.put("pageUtil", pageUtil);
+		
+		return result;
+	}
 	
 }
--- springstudy/15_Pjt_Blog/src/main/webapp/WEB-INF/views/layout/header.jsp
+++ springstudy/15_Pjt_Blog/src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -13,7 +13,7 @@
 <meta charset="UTF-8">
 <title>${title}</title>
 <script src="${contextPath}/resources/js/jquery-3.6.1.min.js"></script>
-<script src="${contextPath}/resources/js/moment-with-locales"></script>
+<script src="${contextPath}/resources/js/moment-with-locales.js"></script>
 <script src="${contextPath}/resources/summernote-0.8.18-dist/summernote-lite.js"></script>
 <script src="${contextPath}/resources/summernote-0.8.18-dist/lang/summernote-ko-KR.min.js"></script>
 <link rel="stylesheet" href="${contextPath}/resources/summernote-0.8.18-dist/summernote-lite.css">  <!-- css는 link로~ -->
